a7dbf497378918f1bccd4d9521e1710b246caeaf
Shabda Raaj <shabda@agiliq.com>
2013-12-08 14:24:14 +0530

    Added more choices

--- js/mason.js
+++ js/mason.js
@@ -32,7 +32,12 @@ $(document).ready(function(){
             ["forms", ["crispy-forms", "parsley", "uni-form"]],
             ["testing", ["nose", "coverage", "milkman"]],
             ["deployment", ["farbic", "salt", "ainsible"]],
-            ["build", ["jenkins", "travis", "circle-ci"]]
+            ["build", ["jenkins", "travis", "circle-ci"]],
+            ["admin", ["Grapelli", "admin2"]],
+            ["debug", ["ipdb", "debug_toolbar", "sentry", "django_extensions"]],
+            ["api", ["piston", "tastypie", "rest_framework"]],
+            ["auth", ["guardian", "userena", "secure_login"]]
+
         ]
 
     var appTypes = ["common", "forms", "testing", "deployment", "build"]
@@ -43,11 +48,13 @@ $(document).ready(function(){
     };
     var appTmpl = _.template('<label class="checkbox"><input checked="checked" type="checkbox" value="{{ appName }}">{{ appName }}</label>')
     var linkTmpl = _.template('<li><a href="#{{ appType }}" class="btn"><i class="icon-chevron-right"></i>{{ appType }}</a></li>');
+    var appTypeTmpl = _.template('<h2>{{ appType }}</h2><section id="{{ appType }}" class="row"><a name="#{{ appType }}"></a></section>');
 
     _(allApps).each(function(appList){
         var appType = appList[0],
             apps = appList[1];
         $("#top-nav").append(linkTmpl({appType: appType}));
+        $("#mason-form").append(appTypeTmpl({appType: appType}));
         _(apps).each(function(appName){
             $("#"+appType).append(appTmpl({appName: appName}));
         });
