c86a0c518ee2f1257be75953ff6ab2eb1c19ec76
Shabda Raaj <shabda@agiliq.com>
2013-12-08 12:05:36 +0530

    Calculate dom in js

---
+++ js/mason.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    "use strict"
+    /*
+    Database ✓
+    Admin ✓
+    South ✓
+    Django Debug Toolbar ✓
+    Fabric ✓
+    Sentry ✓
+    Parsley ✓
+    Travis ✓
+    Merchant
+    Nose
+    Piston
+    Tastypie
+    Rest-framework
+    Userena
+    Guardian
+    Django-CMS
+    Fein-CMS
+    Mezzanine
+    Disqus ✓
+    Django-Extensions
+    Grapelli ✓
+    uni-form
+    crispy-forms ✓
+    */
+
+    var common = ["Database", "Admin", "South"],
+        forms = ["crispy-forms", "parsley", "uni-form"],
+        testing = ["nose", "coverage", "milkman"],
+        deployment = ["farbic", "salt", "ainsible"];
+    _.templateSettings = {
+      interpolate : /\{\{(.+?)\}\}/g
+    };
+    var appTmpl = _.template('<label class="checkbox"><input checked="checked" type="checkbox" value="{{ appName }}">{{ appName }}</label>')
+
+    _(common).each(function(appName){
+        $("#common").append(appTmpl({appName: appName}));
+    });
+
+    _(forms).each(function(appName){
+        $("#forms").append(appTmpl({appName: appName}));
+    });
+
+    _(testing).each(function(appName){
+        $("#testing").append(appTmpl({appName: appName}));
+    });
+
+    _(deployment).each(function(appName){
+        $("#deployment").append(appTmpl({appName: appName}));
+    });
+
+
+});
